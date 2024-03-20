b0c782d5fa8eabf33fd92181a2eb7bd966039798
Murray5577 <limeyi35@gmail.com>
2024-03-20 18:47:47 +0800

    url-shortener2

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const {engine} = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const fs = require('fs')
+
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
@@ -17,10 +19,12 @@ app.get('/shorturl',(req,res)=>{
 })
 
 app.get('/shorten',(req,res)=>{
-    res.render('short')
+    const inputurl = req.query.url
+    const shortUrl = shorten(inputurl)
+    res.render('short',{shortUrl})
 })
 
-//不一定要
+//修改
 app.get('/shorturl/:id', (req, res) => {
     const id = req.params.id
     res.send(`read shorturl: ${id}`)
@@ -30,3 +34,42 @@ app.listen(port,()=>{
     console.log(`express server is running on http://localhost:${port}`)
 })
 
+
+function randomNum(){
+    const number = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let word = ''
+    for (let i=0 ; i<5 ; i++) {
+        word += number[Math.floor(Math.random()*62)]
+    }
+    return word
+}
+
+let newUrl = {
+    "id": "5d6gh",
+    "original": "www.yahoo.com"
+}
+
+function shorten(inputurl){
+    fs.readFile('./url.json', function (err, urlInfo){
+        if (err) { 
+            return console.error(err)
+        }
+
+        let id = randomNum()
+        newUrl.push({
+            id: id,
+            original: inputurl
+        })
+
+        let str = JSON.stringify(newUrl);
+        //將字串符傳入您的 json 文件中
+        fs.writeFile('./url.json', str, function (err) {
+            if (err) {
+                console.error(err);
+            }
+            console.log('Add new url to urlInfo...')
+        })
+    })
+    
+    return `http://localhost:3000/${randomNum()}`
+}
